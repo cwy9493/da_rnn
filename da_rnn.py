bacf6ab83308ec1a623fe284ec8a4bb35521a583
cwy9493 <602260813@qq.com>
2018-12-18 21:29:10 +0800

    import

--- da_rnn.py
+++ da_rnn.py
@@ -5,3 +5,9 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+class Encoder(nn.module):
+    def __init__(self, T, input_size, hidden_size):
+        super(Encoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.T = T
